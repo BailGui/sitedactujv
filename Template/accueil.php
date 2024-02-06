8d5bbe4427d60aad893587a183cfb321fd7f7f90
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-06 14:19:33 +0100

    maj060224/2

--- Template/accueil.php
+++ Template/accueil.php
@@ -11,9 +11,36 @@
 <header><?php include('../Template/inc/bannière.php'); ?></header>
 
   <div class="accueil">
-   <a class="img1" href="indexnewsvr.php"><img class="section" src="../Public/CSS/images/sectionvr.png" alt="section vr"></a>
-   <a class="img2" href="indexnwsflat.php"><img class="section" src="../Public/CSS/images/sectionflat.png" alt="section flat"></a>
+   <a class="img1" href="indexnewsvr.php"><img class="section" src="../Public/CSS/images/sectionvr.png" alt="section vr" id="imgHere"></a>
+   <a class="img2" href="indexnwsflat.php"><img class="section" src="../Public/CSS/images/sectionflat.png" alt="section flat" id="imgHere2"></a>
   </div>
+  
+  <script>
+        function displayScreenWidth() {
+        let theWidth = window.innerWidth;
+
+        if (theWidth > 683) {
+            document.getElementById("imgHere").src = "../Public/CSS/images/sectionvr.png";
+        }else{
+            document.getElementById("imgHere").src = "../Public/CSS/images/vrsmart.png";
+        }
+    }
+        displayScreenWidth();    
+    window.addEventListener('resize', displayScreenWidth);
+    </script>
+
+    <script>
+        function displayScreenWidth() {
+        let theWidth = window.innerWidth;
+        if (theWidth > 683) {
+            document.getElementById("imgHere2").src = "../Public/CSS/images/sectionflat.png";
+        }else{
+            document.getElementById("imgHere2").src = "../Public/CSS/images/flatsmart.png";
+        }
+    }
+        displayScreenWidth();    
+    window.addEventListener('resize', displayScreenWidth);
+    </script>
 
 <footer><?php include('../Template/inc/footer.php'); ?></footer>
 
