14bb8f54cc7921e9f6aea0cd1670e42471689834
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-07 13:28:55 +0100

    maj0702241

--- Template/inc/footer.php
+++ Template/inc/footer.php
@@ -3,5 +3,5 @@
 </div>
 
 <div id='Copyright' >
-<img src="../Public/CSS/images/Copyright.png" alt="copyright" id="copyright">
+<img href="contacts.php" src="../Public/CSS/images/Copyright.png" alt="copyright" id="copyright">
 </div>
